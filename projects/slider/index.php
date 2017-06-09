50617975dafab5511e4ff1decb148f451235de06
lara4594 <milara@csumb.edu>
2017-06-09 22:24:13 +0000

    slider first draft

--- projects/slider/index.php
+++ projects/slider/index.php
@@ -1,28 +1,95 @@
 <?php
 
-include '';
+$backgroundImage = "img/sea.jpg";
+
+if (isset($_GET['keyword'])) {
+    include 'api/pixabayAPI.php';
+    $imageURLs = getImageURLs($_GET['keyword']);
+    $backgroundImage = $imageURLs[array_rand($imageURLs)]; //gets random image URL from the array
+}
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title> </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <style>
-        body{
-            background-image: url('http://c1.staticflickr.com/4/3733/13627757354_d0fba56019_b.jpg');
-            opacity:0.2;
-            width:100%;
-        }
-</style>
+            body{
+                background-image: url('<?=$backgroundImage?>');
+                opacity:0.9;
+                width:100%;
+                text-align:center;
+            }
+            input {
+                font-size: 3em;
+            }
+            #carousel-example-generic {
+                 margin: 0 auto;   
+                 width: 500px;
+            }
+        </style>
     </head>
     <body>
-        
+        <br/> <br />
+        <form>
+            
+            <input type="text" name="keyword" placeholder="Keyword">
+            <input type="submit" value="Submit" />
+            
+        </form>
+        <br/> <br />
         <?php
-
-$imageURLs = getImageURLs("ocean");
-for ($i = 0; $i < 50; $i++) {
-    echo "<img src='" . $imageURLs[$i] . "'  width='200' >";
-}
+            if (isset($imageURLs)) {
+                shuffle($imageURLs); //need to change, too expensive
+               /*     for ($i = 0; $i < 5; $i++) {
+                        echo "<img src='" . $imageURLs[$i] . "'  width='200' >";
+                    }*/
+            ?>
+            
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              </ol>
+            
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img src="<?=$imageURLs[0]?>" alt="...">
+                  <div class="carousel-caption">
+                    ...
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="<?=$imageURLs[1]?>" alt="...">
+                  <div class="carousel-caption">
+                    ...
+                  </div>
+                </div>
+                ...
+              </div>
+            
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          
+            
+            
+                    
+        <?php            
+            }
 
 ?>
 
