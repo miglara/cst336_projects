43e21ae42bb1a0f0e3cbbeed6bf3274a5ce93849
lara4594 <milara@csumb.edu>
2017-09-09 05:16:06 +0000

    adding compressed file

--- projects/csumb_quiz/verifyUser.php
+++ projects/csumb_quiz/verifyUser.php
@@ -4,12 +4,14 @@ session_start();
 include 'connect.php';
 $connect = getDBConnection();
 
-$sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+$sql = "SELECT * FROM users 
+        WHERE username = :username 
+          AND password = :password";
 $stmt = $connect->prepare($sql);
 $data = array(":username" => $_POST['username'], ":password" => sha1($_POST['password']));
 $stmt->execute($data);
-
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
 if(isset($user['username'])){
     $_SESSION['username'] = $user['username'];
     header('Location: index.php');
