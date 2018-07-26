89a901eea28950592d389340cd6dba3ca2643e7d
lara4594 <milara@csumb.edu>
2018-07-26 04:56:01 +0000

    updating pets dbConnection

--- projects/pets/dbConnection.php
+++ projects/pets/dbConnection.php
@@ -6,6 +6,15 @@ function getDatabaseConnection($dbname = 'pets') {
     $username = 'mloza';
     $password = '';
     
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+    
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
