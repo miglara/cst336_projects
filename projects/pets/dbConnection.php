d5d0d8c7397de4737400da48495279d971b24148
lara4594 <milara@csumb.edu>
2018-07-26 04:50:26 +0000

    adding pet shelter

---
+++ projects/pets/dbConnection.php
@@ -0,0 +1,17 @@
+<?php
+
+function getDatabaseConnection($dbname = 'pets') {
+    $host = 'localhost';
+    
+    $username = 'mloza';
+    $password = '';
+    
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}
+
+
+?>
