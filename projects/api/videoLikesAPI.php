579c7f522baee2d0348e3e7a773c92a951d0a7b6
lara4594 <milara@csumb.edu>
2019-03-02 22:10:19 +0000

    adding api folder

---
+++ projects/api/videoLikesAPI.php
@@ -0,0 +1,22 @@
+<?php
+
+//File used by the "video likes" challenge/practice activity.
+//with this proxy, users can keep using the "https" protocol even though the AJAX calls use "http"
+
+    /* proxy.php */
+    $parameters ="?";
+    if (isset($_GET['videoId'])) {
+       $parameters .="videoId=". $_GET['videoId'];
+    }
+    if (isset($_GET['action'])) {
+       $parameters .="&action=". $_GET['action'];
+    }    
+    $url = "http://myspace.csumb.edu/~milara/ajax/videosAPI.php". $parameters;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec ($ch);
+    curl_close ($ch);
+    echo $result;
+
+?>
