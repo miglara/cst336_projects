47907ad87b2f20915cf93b94499a2234b346fc26
lara4594 <milara@csumb.edu>
2019-05-06 04:23:42 +0000

    adding apis

---
+++ projects/api/countyListAPI.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+//File used by the "sign up" lab.
+//with this proxy, users can keep using the "https" protocol even though the AJAX calls use "http"
+
+    /* proxy.php */
+    $parameters ="?";
+    if (isset($_GET['state'])) {
+       $parameters .="state=". $_GET['state'];
+    }
+    $url = "http://itcdland.csumb.edu/~milara/ajax/countyList.php". $parameters;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec ($ch);
+    curl_close ($ch);
+    echo $result;
+
+?>
