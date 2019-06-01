574c472f7d9163a27ecc63e6f83f67599f75e4c0
lara4594 <milara@csumb.edu>
2019-06-01 17:41:36 +0000

    state api

---
+++ projects/api/state_abbrAPI.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+//File used by the "sign up" lab.
+//with this proxy, users can get the list of all states with their two-letter abbreviation
+
+
+    $url = "http://itcdland.csumb.edu/~milara/ajax/states.php";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec ($ch);
+    curl_close ($ch);
+    echo $result;
+
+?>
