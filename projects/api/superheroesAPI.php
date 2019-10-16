d1e93183bdc8634cedbde70700f35947d4677ca0
miglara <milara@csumb.edu>
2019-10-15 23:11:49 -0700

    Create superheroesAPI.php

---
+++ projects/api/superheroesAPI.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+//File used by the "superheroes" midterm EXAM using AJAX.
+//with this proxy, users can keep using the "https" protocol even though the AJAX calls use "http"
+    /* proxy.php */
+    $parameters ="?";
+    if (isset($_GET['heroId'])) {
+       $parameters .="heroId=". $_GET['heroId'];
+    }
+    if (isset($_GET['data'])) {
+       $parameters .="&data=". $_GET['data'];
+    }    
+    if (isset($_GET['pop'])) {
+       $parameters .="&pop=". $_GET['pop'];
+    }  
+    $url = "http://myspace.csumb.edu/~milara/ajax/superheroes/superheroesAPI.php". $parameters;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec ($ch);
+    curl_close ($ch);
+    echo $result;
+?>
