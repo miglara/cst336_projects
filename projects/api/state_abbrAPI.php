23592e5dd9f88caa68b6032d4b4947b513344e98
miglara <milara@csumb.edu>
2020-07-05 23:19:10 -0500

    Update state_abbrAPI.php

--- projects/api/state_abbrAPI.php
+++ projects/api/state_abbrAPI.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 //File used by the "sign up" lab.
 //with this proxy, users can get the list of all states with their two-letter abbreviation
 
-
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //allows connecting to https
     $url = "https://itcdland.csumb.edu/~milara/ajax/states.php";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
