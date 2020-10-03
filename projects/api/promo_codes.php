b4632951c27e10b0d7ae57fbf19c6bc510cd7bb5
miglara <milara@csumb.edu>
2020-10-02 23:12:12 -0500

    Update promo_codes.php

--- projects/api/promo_codes.php
+++ projects/api/promo_codes.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 //File used by the "Promo Codes" challenge/practice activity.
 //with this proxy, users can keep using the "https" protocol even though the AJAX calls use "http"
     
-    $url = "https://myspace.csumb.edu/~milara/ajax/promo/promoCode.php?promoCode=" . $_GET['promoCode'];
+    $url = "https://itcdland.csumb.edu/~milara/ajax/promo/promoCode.php?promoCode=" . $_GET['promoCode'];
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //allows connecting to https
