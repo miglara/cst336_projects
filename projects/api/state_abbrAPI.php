dcccc2df511093d238b4fdba7aa6a574045c7240
miglara <milara@csumb.edu>
2020-11-12 23:30:52 -0600

    Update state_abbrAPI.php

--- projects/api/state_abbrAPI.php
+++ projects/api/state_abbrAPI.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 //File used by the "sign up" lab.
 //with this proxy, users can get the list of all states with their two-letter abbreviation
    
-    $url = "https://wikipedia.com";
+    $url = "https://wikipedia.org";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //allows connecting to https
     curl_setopt($ch, CURLOPT_URL, $url);
